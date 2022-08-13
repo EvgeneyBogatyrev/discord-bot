c9bf28feb4bf43835b933c1d8baf366b986a26cb
Евгений Зимин <evgeney.zimin@graphics.cs.msu.ru>
2022-08-13 14:44:12 +0300

    Metagame to .PNG

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import codecs
 import math
 import asyncio
 from random import randint
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 from constants import Constants
 from functions import *
@@ -364,6 +366,7 @@ async def nickname(ctx, message):
         json.dump(data, f)
     await ctx.reply("Your nickname has been updated!")
 
+
 @bot.command()
 async def leaderboard(ctx):
     with codecs.open("data/rating.csv", "r", 'utf-8') as f:
@@ -610,7 +613,88 @@ async def reject(ctx):
 
         
 @bot.command() 
-async def metagame(ctx):
+async def metagame(ctx):       
+    with open("data/metagame.json", "r") as f:
+        meta_data = json.load(f)
+    if os.path.isfile("data/metagame2vs2.json"):
+        with open("data/metagame2vs2.json", "r") as f:
+            meta_data_2 = json.load(f)
+        for key in meta_data.keys():
+            meta_data[key] += meta_data_2[key]
+
+    sort_data = {k: v for k, v in sorted(meta_data.items(), key=lambda item: item[1], reverse=True)}
+    summ = 0
+    for key in meta_data.keys():
+        summ += meta_data[key]
+    if summ == 0:
+        await ctx.reply("Some error has occured. Ask admins to fix it.")
+        return
+
+    ratings = []
+    names = []
+    percent = []
+
+    color = []
+
+    for key in sort_data:
+        if sort_data[key] == 0:
+            continue
+        ratings.append(sort_data[key])
+        names.append(key)
+        percent.append(round(sort_data[key] / summ, 3))
+
+        if key in ["Yarida", "Piekron", "Wooyari", "Kibadda", "Pyokorider", "Cannassault", "Charibasa"]:
+            color.append("#8496ff")
+        elif key in ["Taterazay", "Guardira", "Grenburr", "Tondenga", "Myamsar", "Destrobo", "Bowmunk"]:
+            color.append("#f78a18")
+        elif key in ["Yumiyacha", "Alosson", "Wondabarappa", "Jamsch", "Oohoroc", "Pingrek", "Cannogabang"]:
+            color.append("#9cd721")
+        elif key in ["Ragewolf", "Naughtyfins", "Sonarchy", "Ravenous", "Buzzcrave", "Slogturtle", "Covet-hiss"]:
+            color.append("#b57df7")
+        else:
+            color.append("white")
+
+
+
+    data = {
+        "rating" : ratings,
+        "name" : names,
+        "percent" : percent 
+    }
+
+    fig, ax = plt.subplots()
+
+    
+    fig.patch.set_visible(False)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    io = sns.barplot(data=data, x="rating", y="name", ax=ax, palette=color)
+    ax.bar_label(ax.containers[-1], fmt=' %d', label_type='edge', color='snow', fontsize=14)
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+
+    for i, bar in enumerate(io.patches):
+        io.annotate(names[i] + " - ",
+                    (0,
+                         bar.get_y() + bar.get_height()/2 + 0.07), ha='right', va='top',
+                    size=11, xytext=(0, 8),
+                    textcoords='offset points', color="snow")
+
+    plt.tight_layout()
+    plt.savefig('metagame.png', transparent=True)
+
+    with open('metagame.png', 'rb') as f:
+        picture = discord.File(f)
+        await ctx.send(file=picture)
+
+    
+    
+
+@bot.command() 
+async def metagame_old(ctx):
     with open("data/status.txt", "r") as f:
         STATUS = f.readline()
     with open("data/metagame.json", "r") as f:
